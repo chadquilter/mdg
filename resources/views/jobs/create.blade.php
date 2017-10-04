0e680594ad2fd3d15c8ea0f771fdece326556ff1
Chad Quilter <chadquilter@hotmail.com>
2017-10-04 01:04:51 -0500

    add initial drawing tool

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -16,6 +16,9 @@
             {{Form::label('job_notes', 'Notes:')}}
             {{Form::text('job_notes', '', ['class' => 'form-control', 'placeholder' => 'Job Additional Notes'])}}
         </div>
+
+        <div id="canvasDiv"></div>
+
         <div class="form=group">
             {{Form::label('job_type', 'Job Type:')}}
             </br>
@@ -25,7 +28,7 @@
                 @endforeach
             @else
                 <h1>No Types Listed!</h1>
-            @endif    
+            @endif
         </div>
         @if (count($job_option_types) > 0)
             <div class="form=group">
@@ -34,7 +37,7 @@
 </br>
                 @if(count($bool_types) > 0)
                     @foreach($bool_types as $bool_id => $bool_name)
-                        {{Form::radio($job_option_id, $bool_id, ['class' => 'form-control'])}} {{$bool_name}} 
+                        {{Form::radio($job_option_id, $bool_id, ['class' => 'form-control'])}} {{$bool_name}}
                     @endforeach
                 @else
                     <h1>No Types Listed!</h1>
@@ -44,7 +47,7 @@
             </div>
         @endif
         </br>
-        
+
         @if (count($users) > 0)
         <div class="form=group">
             {{Form::label('job_created_by', 'Job Created by:')}}
@@ -56,7 +59,76 @@
             </div>
             </br>
         @endif
-        
+        <script type="javascript">
+          var canvasDiv = document.getElementById('canvasDiv');
+          canvas = document.createElement('canvas');
+          canvas.setAttribute('width', canvasWidth);
+          canvas.setAttribute('height', canvasHeight);
+          canvas.setAttribute('id', 'canvas');
+          canvasDiv.appendChild(canvas);
+          if(typeof G_vmlCanvasManager != 'undefined') {
+            canvas = G_vmlCanvasManager.initElement(canvas);
+          }
+          context = canvas.getContext("2d");
+
+          $('#canvas').mousedown(function(e){
+            var mouseX = e.pageX - this.offsetLeft;
+            var mouseY = e.pageY - this.offsetTop;
+
+            paint = true;
+            addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+            redraw();
+          });
+
+          $('#canvas').mousemove(function(e){
+            if(paint){
+              addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
+              redraw();
+            }
+          });
+
+          $('#canvas').mouseup(function(e){
+            paint = false;
+          });
+
+          $('#canvas').mouseleave(function(e){
+            paint = false;
+          });
+
+          var clickX = new Array();
+          var clickY = new Array();
+          var clickDrag = new Array();
+          var paint;
+
+          function addClick(x, y, dragging)
+          {
+            clickX.push(x);
+            clickY.push(y);
+            clickDrag.push(dragging);
+          }
+
+          function redraw(){
+  context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
+
+  context.strokeStyle = "#df4b26";
+  context.lineJoin = "round";
+  context.lineWidth = 5;
+
+  for(var i=0; i < clickX.length; i++) {
+    context.beginPath();
+    if(clickDrag[i] && i){
+      context.moveTo(clickX[i-1], clickY[i-1]);
+     }else{
+       context.moveTo(clickX[i]-1, clickY[i]);
+     }
+     context.lineTo(clickX[i], clickY[i]);
+     context.closePath();
+     context.stroke();
+  }
+}
+
+
+        </script>
 
         {{Form::submit('Submit', ['class=btn btn-primary'])}}
         {!! Form::close() !!}
