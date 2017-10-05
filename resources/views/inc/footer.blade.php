9392656a0fcc4e2451774b10db5034d65be094a2
Chad Quilter <chadquilter@hotmail.com>
2017-10-05 12:18:46 -0500

    add footer

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -1,1 +1,8 @@
 
+<div class="footer-area">
+  <div class="footer-block">
+
+  </div>
+</div>
+
+<div class="fusion-separator fusion-full-width-sep sep-shadow" style="background:radial-gradient(ellipse at 50% -50% , #e0dede 0px, rgba(255, 255, 255, 0) 80%) repeat scroll 0 0 rgba(0, 0, 0, 0);background:-webkit-radial-gradient(ellipse at 50% -50% , #e0dede 0px, rgba(255, 255, 255, 0) 80%) repeat scroll 0 0 rgba(0, 0, 0, 0);background:-moz-radial-gradient(ellipse at 50% -50% , #e0dede 0px, rgba(255, 255, 255, 0) 80%) repeat scroll 0 0 rgba(0, 0, 0, 0);background:-o-radial-gradient(ellipse at 50% -50% , #e0dede 0px, rgba(255, 255, 255, 0) 80%) repeat scroll 0 0 rgba(0, 0, 0, 0);margin-left: auto;margin-right: auto;margin-top:20px;margin-bottom:50px;"><span class="icon-wrapper" style="border-color:#e0dede;"><i class="fa fa-leaf" style="color:#e0dede;"></i></span></div>
