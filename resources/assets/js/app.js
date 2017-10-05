9fb6ab2f824f984a35514b467191e3e988d57b43
Chad Quilter <chadquilter@hotmail.com>
2017-10-05 18:25:00 -0500

    add sprockets

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -10,7 +10,7 @@ require('./bootstrap');
 try {
     window.$ = window.jQuery = require('jquery');
     window.Popper = require('popper.js').default;
-    require('bootstrap');
+    require('bootstrap-sass');
     require('jquery-ui');
 } catch (e) {}
 
