4abc79aca253ffac450d56d8b5eaf965f7fac626
Chad Quilter <chadquilter@hotmail.com>
2017-10-06 16:44:22 -0500

    add images

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -9,7 +9,9 @@ text-align: center;
 }
 
 div.image_display_r {
-  align: right;
+  width: 886px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  text-align: center;
 }
 
 div.image_display_text_container {
@@ -31,7 +33,7 @@ padding: 10px;
       We are a construction company based out of marble falls, TX, that specializes in Custom Homes and Additions, Home Repair and Pavement.
     </p>
     </br>
-    <div class="image_display image_display_r">
+    <div class="image_display_r">
       <img class="image_for_display" style="width=100%;" alt="A scenic home image" src="/images/6_orig.jpg">
       <div class="image_display_text_container">
         <p>Our work is a cut above the rest!</p>
