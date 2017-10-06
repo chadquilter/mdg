9fc4ba2c3b4993c7d25a4121427d9466bfe3cc16
Chad Quilter <chadquilter@hotmail.com>
2017-10-06 17:00:48 -0500

    add images

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -5,7 +5,12 @@
   <div class="foot-container">
     <div class="footer-text" styel="width:70%;">
       <center>
-        <p class="text-muted">Place sticky footer content here.</p>
+        <p class="text-muted">
+          </br>
+          &copy; 2017 A Cut Above Construction
+          </br>
+          &trade; A Cut Above Contruction is an Equal Opportunity Employer.
+        </p>
       </center>
     </div>
     <div class="footer-badges" style="width:30%;">
