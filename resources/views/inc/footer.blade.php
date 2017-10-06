f986793d6661453290366bc252bf74b45ba1991d
Chad Quilter <chadquilter@hotmail.com>
2017-10-06 16:55:35 -0500

    add images

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -4,7 +4,9 @@
   </div>
   <div class="foot-container">
     <div class="footer-text" styel="width:70%;">
-      <p class="text-muted">Place sticky footer content here.</p>
+      <center>
+        <p class="text-muted">Place sticky footer content here.</p>
+      </center>
     </div>
     <div class="footer-badges" style="width:30%;">
       <p clas="badges">test test</p>
