94c47789d6118dbda3fb2cfd4b785fa40d709ffd
Chad Quilter <chadquilter@hotmail.com>
2017-10-06 17:28:02 -0500

    add images

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -7,14 +7,19 @@
       <center>
         <p class="text-muted">
           </br>
-          &copy; 2017 A Cut Above Construction
+          &copy; 2017, A Cut Above Construction 55 Inc.,
           </br>
-          &trade; A Cut Above Contruction is an Equal Opportunity Employer.
+          &trade; "A Cut Above Contruction" is an Equal Opportunity Employer.
+          All Rights Reserved.
         </p>
       </center>
     </div>
     <div class="footer-badges" style="width:30%;">
       <p clas="badges">test test</p>
+      <image src="http://www.souppahomeimprovement.com/wp-content/uploads/2013/05/bbb-icon.png" style="width=100%;">
+      <image src="http://marblefallstri.com/MF_Chamber_Logo.jpg" style="width=100%;">
+
+
     </div>
     <div class="footer-bottom">
       &nbsp
