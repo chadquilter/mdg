4e1390e2898b59690cf76ed09a00fe2060332570
Chad Quilter <chadquilter@hotmail.com>
2017-10-06 15:12:29 -0500

    add new front end layout

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <style>
 div.image_display {
-width: 550px;
+width: 640px;
 box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 text-align: center;
 }
