a39cd0b170c0c285e94c66a486ba757442d838df
Chad Quilter <chadquilter@hotmail.com>
2017-10-07 00:35:49 -0500

    add images

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -29,9 +29,12 @@ div.image_display_text_container {
     <div class="container">
     <h1>{{$title}}</h1>
     <p>
-      Welcome!
+      Cut above Construction has helped thousands of happy homeowners across Texas build the new custom-designed home of their dreams.
+      We build "eco-friendly" green custom homes of all sizes for all budgets. Whatever custom home you have in mind, from a quaint cottage in Austin,
+      a rambling farm house in the Hill Country or an elegant luxury estate home in Houston, Dallas or San Antonio, we can build, repair, or add to it at an affordable price.
       </br>
-      We are a construction company based out of marble falls, TX, that specializes in Custom Homes and Additions, Home Repair and Pavement.
+      As a Texas custom home builder for over 10 years, we have made thousands of happy customers' dream home a reality by delivering on that promise.
+      We would love to help you! For a free custom design meeting, contact us today. Let's get started on your new, custom dream home or new addition now!
     </p>
     </br>
     <div class="image_display">
