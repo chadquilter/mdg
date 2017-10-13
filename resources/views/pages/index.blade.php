f69ce96944f06c570c0ab114f58041e6e8c8c556
Chad Quilter <chadquilter@hotmail.com>
2017-10-13 16:14:14 -0500

    commit css updates

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,29 +1,31 @@
 @extends('layouts.front')
 
 @section('content')
-    <div class="image_display_r">
-      <img class="image_for_display img-fluid" alt="A scenic home image" src="/images/dusk-home.jpg">
-      <div class="image_display_text_container">
-        <p>Our work is a cut above the rest!</p>
-      </div>
-    </div>
-    </br>
+
     <div class="container">
-    <h1>{{$title}}</h1>
-    <p>
-      Cut above Construction has helped thousands of happy homeowners across Texas build the new custom-designed home of their dreams.
-      We build "eco-friendly" green custom homes of all sizes for all budgets. Whatever custom home you have in mind, from a quaint cottage in Austin,
-      a rambling farm house in the Hill Country or an elegant luxury estate home in Houston, Dallas or San Antonio, we can build, repair, or add to it at an affordable price.
-    </p>
-    <p>
-      As a Texas custom home builder for over 10 years, we have made thousands of happy customers' dream home a reality by delivering on that promise.
-      We would love to help you! For a free custom design meeting, contact us today. Let's get started on your new, custom dream home or new addition now!
-    </p>
-    </br>
-    <div class="image_display">
-      <img class="image_for_display img-fluid"  alt="A scenic home image" src="/images/mdg_images/IMG_0022.JPG">
-      <div class="image_display_text_container">
-        <p>Custom Design!</p>
+      <div class="image_display_r">
+        <img class="image_for_display img-fluid" alt="A scenic home image" src="/images/dusk-home.jpg">
+        <div class="image_display_text_container">
+          <p>Our work is a cut above the rest!</p>
+        </div>
+      </div>
+      </br>
+      <h1>{{$title}}</h1>
+      <p>
+        Cut above Construction has helped thousands of happy homeowners across Texas build the new custom-designed home of their dreams.
+        We build "eco-friendly" green custom homes of all sizes for all budgets. Whatever custom home you have in mind, from a quaint cottage in Austin,
+        a rambling farm house in the Hill Country or an elegant luxury estate home in Houston, Dallas or San Antonio, we can build, repair, or add to it at an affordable price.
+      </p>
+      <p>
+        As a Texas custom home builder for over 10 years, we have made thousands of happy customers' dream home a reality by delivering on that promise.
+        We would love to help you! For a free custom design meeting, contact us today. Let's get started on your new, custom dream home or new addition now!
+      </p>
+      </br>
+      <div class="image_display">
+        <img class="image_for_display img-fluid"  alt="A scenic home image" src="/images/mdg_images/IMG_0022.JPG">
+        <div class="image_display_text_container">
+          <p>Custom Design!</p>
+        </div>
       </div>
     </div>
     </br>
