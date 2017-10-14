7ea49a4a901983686a8006b8f726cdcda5fef867
Chad Quilter <chadquilter@hotmail.com>
2017-10-14 02:08:00 -0500

    commit css updates

--- resources/views/pages/quote.blade.php
+++ resources/views/pages/quote.blade.php
@@ -11,7 +11,7 @@
 		</div>
 	</div>
 	<div class="col-md-6">
-		<div class="image_display_r col-md-6">
+		<div class="image_display_r">
 			<img class="img-fluid" alt="A scenic home image" src="/images/mdg_images/IMG_0017.JPG" style="max-height:450%; width: 100%; display: block;">
 			<div class="image_display_text_container">
 				<p>Our work is a cut above the rest!</p>
