9886c6655750ee640481b85dd7fb1dc5e3c5a5ec
Chad Quilter <chadquilter@hotmail.com>
2017-10-14 02:07:05 -0500

    commit css updates

--- resources/views/pages/quote.blade.php
+++ resources/views/pages/quote.blade.php
@@ -2,21 +2,22 @@
 
 @section('content')
 	<div class="row">
+	<div class="col-md-6">
 		<div class="image_display_r">
 			<img class="img-fluid" alt="A scenic home image" src="/images/mdg_images/IMG_0033.JPG" style="max-height:450%; width: 100%; display: block;">
 			<div class="image_display_text_container">
 				<p>Our work is a cut above the rest!</p>
 			</div>
 		</div>
-		<div class="col-md-6">
-			test
-		</div>
+	</div>
+	<div class="col-md-6">
 		<div class="image_display_r col-md-6">
 			<img class="img-fluid" alt="A scenic home image" src="/images/mdg_images/IMG_0017.JPG" style="max-height:450%; width: 100%; display: block;">
 			<div class="image_display_text_container">
 				<p>Our work is a cut above the rest!</p>
 			</div>
 		</div>
+	</div>
 	</div>
 	</br>
 	<div class="container well">
