530ef6d4a244099ed240f27856a3eeb201c5f57c
Chad Quilter <chadquilter@hotmail.com>
2017-10-13 21:00:35 -0500

    commit css updates

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -2,7 +2,7 @@
          <nav class="navbar navbar-inverse">
                  <div class="navbar-header">
                      <!-- Collapsed Hamburger -->
-                     <button type="button" class="navbar-toggle collapsed" data-        toggle="collapse" data-target="#app-navbar-collapse">
+                     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
                          <span class="sr-only">Toggle Navigation</span>
                          <span class="icon-bar"></span>
                          <span class="icon-bar"></span>
@@ -11,7 +11,7 @@
 
                      <!-- Branding Image -->
                      <a class="navbar-brand" href="{{ url('/') }}">
-                         {{ config('app.name', 'Laravel') }}
+                         {{ config('app.name', 'MDG') }}
                      </a>
                  </div>
                  <div class="collapse navbar-collapse" id="app-navbar-collapse">
