758baef03abf949c5f4530f95238e0f1e0ce7af2
Chad Quilter <chadquilter@hotmail.com>
2017-10-15 18:42:15 -0500

    commit updates

--- resources/views/pages/quote.blade.php
+++ resources/views/pages/quote.blade.php
@@ -3,60 +3,52 @@
 
 @section('content')
 	<div class="row">
-	<div class="col-md-6">
-		<div class="image_display_r">
-			<img class="img-fluid" alt="A scenic home image" src="/images/mdg_images/IMG_0033.JPG" style="max-height:450%; width: 100%; display: block;">
-			<div class="image_display_text_container">
-				<p>Our work is a cut above the rest!</p>
+		<div class="col-md-6">
+			<div class="image_display_r">
+				<img class="img-fluid" alt="A scenic home image" src="/images/mdg_images/IMG_0033.JPG" style="max-height:450%; width: 100%; display: block;">
+				<div class="image_display_text_container">
+					<p>Our work is a cut above the rest!</p>
+				</div>
 			</div>
 		</div>
-	</div>
-	<div class="col-md-6">
-		<div class="image_display_r">
-			<img class="img-fluid" alt="A scenic home image" src="/images/mdg_images/IMG_0017.JPG" style="max-height:450%; width: 100%; display: block;">
-			<div class="image_display_text_container">
-				<p>Our work is a cut above the rest!</p>
+		<div class="col-md-6">
+			<div class="image_display_r">
+				<img class="img-fluid" alt="A scenic home image" src="/images/mdg_images/IMG_0017.JPG" style="max-height:450%; width: 100%; display: block;">
+				<div class="image_display_text_container">
+					<p>Our work is a cut above the rest!</p>
+				</div>
 			</div>
 		</div>
-	</div>
 	</div>
 	</br>
 	<div class="container well">
 		<h1>Create a Quote:</h1>
 		{!! Form::open(['action' => 'JobsController@store', 'method' => 'POST', 'enctype' => 'multipart/data']) !!}
-				<div class="form=group">
-						{{Form::label('quote_title', 'Quote Title:')}}
-						{{Form::text('quote_title', '', ['class' => 'form-control', 'placeholder' => 'Quote Title'])}}
-				</div>
-				<div id="app-6" class="form=group">
-						{{Form::label('quote_phone', 'Phone:')}}
-						{{Form::text('quote_phone', '', ['class' => 'form-control', 'placeholder' => 'Phone', 'v-model' => 'message'])}}
-				</div>
-				<div class="form=group">
-						{{Form::label('quote_email', 'Email:')}}
-						{{Form::text('quote_email', '', ['class' => 'form-control', 'placeholder' => 'Email'])}}
-				</div>
-				<div class="form=group">
-						{{Form::label('quote_summary', 'Summary:')}}
-						{{Form::textarea('quote_summary', '', ['id' => 'article-ckeditor', 'class' => 'form-control', 'placeholder' => 'Quote Summary'])}}
-				</div>
-				<div class="form=group">
-						{{Form::label('quote_notes', 'Quote Notes:')}}
-						{{Form::text('quote_notes', '', ['class' => 'form-control', 'placeholder' => 'Quote Additional Notes'])}}
-				</div>
-				{{Form::submit('Submit', ['class=btn btn-primary'])}}
-				{!! Form::close() !!}
-				<script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
-				<script>
-						CKEDITOR.replace( 'article-ckeditor' );
-				</script>
-				<script>
-								var app6 = new Vue({
-									el: '#app-6',
-										data: {
-										message: 'Phone Number!'
-									}
-								})
-				</script>
+			<div class="form=group">
+					{{Form::label('quote_title', 'Quote Title:')}}
+					{{Form::text('quote_title', '', ['class' => 'form-control', 'placeholder' => 'Quote Title'])}}
+			</div>
+			<div id="app-6" class="form=group">
+					{{Form::label('quote_phone', 'Phone:')}}
+					{{Form::text('quote_phone', '', ['class' => 'form-control', 'placeholder' => 'Phone'])}}
+			</div>
+			<div class="form=group">
+					{{Form::label('quote_email', 'Email:')}}
+					{{Form::text('quote_email', '', ['class' => 'form-control', 'placeholder' => 'Email'])}}
+			</div>
+			<div class="form=group">
+					{{Form::label('quote_summary', 'Summary:')}}
+					{{Form::textarea('quote_summary', '', ['id' => 'article-ckeditor', 'class' => 'form-control', 'placeholder' => 'Quote Summary'])}}
+			</div>
+			<div class="form=group">
+					{{Form::label('quote_notes', 'Quote Notes:')}}
+					{{Form::text('quote_notes', '', ['class' => 'form-control', 'placeholder' => 'Quote Additional Notes'])}}
+			</div>
+			{{Form::submit('Submit', ['class=btn btn-primary'])}}
+		{!! Form::close() !!}
+		<script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
+		<script>
+				CKEDITOR.replace( 'article-ckeditor' );
+		</script>
 	</div>
 @endsection
