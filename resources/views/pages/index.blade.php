cb2998d795cd2aea37ca55697c2f63ed78959784
Chad Quilter <chadquilter@hotmail.com>
2017-10-15 23:12:34 -0500

    commit updates

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -11,16 +11,18 @@
         </div>
       </div>
       <br>
-      <h1>{{$title}}</h1>
-      <p>
-        Cut above Construction has helped thousands of happy homeowners across Texas build the new custom-designed home of their dreams.
-        We build "eco-friendly" green custom homes of all sizes for all budgets. Whatever custom home you have in mind, from a quaint cottage in Austin,
-        a rambling farm house in the Hill Country or an elegant luxury estate home in Houston, Dallas or San Antonio, we can build, repair, or add to it at an affordable price.
-      </p>
-      <p>
-        As a Texas custom home builder for over 10 years, we have made thousands of happy customers' dream home a reality by delivering on that promise.
-        We would love to help you! For a free custom design meeting, contact us today. Let's get started on your new, custom dream home or new addition now!
-      </p>
+      <div class="well">
+        <h1>{{$title}}</h1>
+        <p>
+          Cut above Construction has helped thousands of happy homeowners across Texas build the new custom-designed home of their dreams.
+          We build "eco-friendly" green custom homes of all sizes for all budgets. Whatever custom home you have in mind, from a quaint cottage in Austin,
+          a rambling farm house in the Hill Country or an elegant luxury estate home in Houston, Dallas or San Antonio, we can build, repair, or add to it at an affordable price.
+        </p>
+        <p>
+          As a Texas custom home builder for over 10 years, we have made thousands of happy customers' dream home a reality by delivering on that promise.
+          We would love to help you! For a free custom design meeting, contact us today. Let's get started on your new, custom dream home or new addition now!
+        </p>
+      </div>
       <br>
       <div class="image_display_r">
         <img class="img-fluid"  alt="A scenic home image" src="/images/mdg_images/IMG_0022.JPG" style="max-height:450%; width: 100%; display: block;">
