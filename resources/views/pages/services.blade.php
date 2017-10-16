4c96bb5dc379f74cbe0d8ca340575427ab51d28e
Chad Quilter <chadquilter@hotmail.com>
2017-10-15 22:33:10 -0500

    commit updates

--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -2,6 +2,25 @@
 
 @section('content')
 	<h1>{{$title}}</h1>
+	<div class="row">
+		<br>
+		<div class="col-md-6">
+			<div class="image_display_r">
+				<img class="img-fluid" alt="A scenic home image" src="/images/mdg_images/IMG_0033.JPG" style="max-height:450%; width: 100%; display: block;">
+				<div class="image_display_text_container">
+					<p>Our work is a cut above the rest!</p>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-6">
+			<div class="image_display_r">
+				<img class="img-fluid" alt="A scenic home image" src="/images/mdg_images/IMG_0017.JPG" style="max-height:450%; width: 100%; display: block;">
+				<div class="image_display_text_container">
+					<p>Our work is a cut above the rest!</p>
+				</div>
+			</div>
+		</div>
+	</div>
 	@if(count($services) > 0)
 		<ul>
 		@foreach($services as $service)
