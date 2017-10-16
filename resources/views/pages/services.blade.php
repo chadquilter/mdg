3311dad3bba95e7e29720159087caeb7d18a544c
Chad Quilter <chadquilter@hotmail.com>
2017-10-16 01:25:07 -0500

    commit updates

--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -13,7 +13,7 @@
 		</div>
 		<div class="col-md-6">
 			<div class="image_display_r">
-				<img class="img-fluid" alt="A scenic home image" src="/images/mdg_images/IMG_0041.JPG" style="max-height:450%; width: 100%; display: block;">
+				<img class="img-fluid" alt="A scenic home image" src="/images/mdg_images/IMG_SHOWER1.JPG" style="max-height:450%; width: 100%; display: block;">
 				<div class="image_display_text_container">
 					<p>Our work is a cut above the rest!</p>
 				</div>
