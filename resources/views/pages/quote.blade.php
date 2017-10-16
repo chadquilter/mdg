1e786aadb34ef96da2237a9a7f69227b95b5c5e0
Chad Quilter <chadquilter@hotmail.com>
2017-10-16 01:36:10 -0500

    commit updates

--- resources/views/pages/quote.blade.php
+++ resources/views/pages/quote.blade.php
@@ -6,7 +6,7 @@
 		<br>
 		<div class="col-md-6">
 			<div class="image_display_r">
-				<img class="img-fluid" alt="A scenic home image" src="/images/mdg_images/IMG_0040.JPG" style="max-height:450%; width: 100%; display: block;">
+				<img class="img-fluid" alt="A scenic home image" src="/images/mdg_images/IMG_BATHROOM2.JPG" style="max-height:450%; width: 100%; display: block;">
 				<div class="image_display_text_container">
 					<p>Our work is a cut above the rest!</p>
 				</div>
@@ -14,7 +14,7 @@
 		</div>
 		<div class="col-md-6">
 			<div class="image_display_r">
-				<img class="img-fluid" alt="A scenic home image" src="/images/mdg_images/IMG_0046.JPG" style="max-height:450%; width: 100%; display: block;">
+				<img class="img-fluid" alt="A scenic home image" src="/images/mdg_images/IMG_F1F2_1.JPG" style="max-height:450%; width: 100%; display: block;">
 				<div class="image_display_text_container">
 					<p>Our work is a cut above the rest!</p>
 				</div>
