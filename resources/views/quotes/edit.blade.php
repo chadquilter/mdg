742a5c0c6fb2cc27d4d2380dc2a59cd40e67ff73
Chad Quilter <chadquilter@hotmail.com>
2017-10-17 00:00:46 -0500

    add new quote form pages

--- resources/views/quotes/edit.blade.php
+++ resources/views/quotes/edit.blade.php
@@ -2,27 +2,27 @@
 
 @section('content')
   <div class=container>
-    <h1>Edit Job</h1>
+    <h1>Edit Quote</h1>
     {!! Form::open(['action' => 'QuotesController@store', 'method' => 'POST', 'enctype' => 'multipart/data']) !!}
       <div class="form=group">
           {{Form::label('title', 'Title:')}}
-          {{Form::text('title', '', ['class' => 'form-control', 'placeholder' => 'Title'])}}
+          {{Form::text('title', $quote->title, ['class' => 'form-control', 'placeholder' => 'Title'])}}
       </div>
       <div id="app-6" class="form=group">
           {{Form::label('phone', 'Phone:')}}
-          {{Form::text('phone', '', ['class' => 'form-control', 'placeholder' => 'Phone'])}}
+          {{Form::text('phone', $quote->phone, ['class' => 'form-control', 'placeholder' => 'Phone'])}}
       </div>
       <div class="form=group">
           {{Form::label('email', 'Email:')}}
-          {{Form::text('email', '', ['class' => 'form-control', 'placeholder' => 'Email'])}}
+          {{Form::text('email', $quote->email, ['class' => 'form-control', 'placeholder' => 'Email'])}}
       </div>
       <div class="form=group">
           {{Form::label('description', 'description:')}}
-          {{Form::textarea('description', '', ['id' => 'article-ckeditor', 'class' => 'form-control', 'placeholder' => 'Quote Description'])}}
+          {{Form::textarea('description', $quote->description, ['id' => 'article-ckeditor', 'class' => 'form-control', 'placeholder' => 'Quote Description'])}}
       </div>
       <div class="form=group">
           {{Form::label('notes', 'Notes:')}}
-          {{Form::text('notes', '', ['class' => 'form-control', 'placeholder' => 'Additional Notes'])}}
+          {{Form::text('notes', $quote->notes, ['class' => 'form-control', 'placeholder' => 'Additional Notes'])}}
       </div>
       <br>
       <div class="form=group">
@@ -34,4 +34,5 @@
         CKEDITOR.replace( 'article-ckeditor' );
     </script>
   </div>
+  <br>
 @endsection
