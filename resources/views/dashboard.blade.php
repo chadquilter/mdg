f7fd16600069a2ebe133e318f525ed648875a2ff
Chad Quilter <chadquilter@hotmail.com>
2017-10-17 00:25:12 -0500

    add new quote form pages

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -58,7 +58,7 @@
                     <table class="table table-striped table-hover table-sm table-responsive">
                         <thead class="thead-inverse">
                         <tr>
-                            <th><h3>Your Quote Entries:</h3></th>
+                            <th><h3>Quote Entries:</h3></th>
                             <th></th>
                             <th></th>
                         </tr>
