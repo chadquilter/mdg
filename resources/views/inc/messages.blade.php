d103e6b45c8204efc429acfaa9be4cd0fe19d38a
Chad Quilter <chadquilter@hotmail.com>
2017-10-17 00:57:38 -0500

    add new quote form pages

--- resources/views/inc/messages.blade.php
+++ resources/views/inc/messages.blade.php
@@ -1,19 +1,19 @@
 @if(count($errors) > 0)
     @foreach($errors->all() as $error)
-        <div class="alert alert-danger">
+        <div class="alert alert-danger image_display_text_container">
             {{$error}}
         </div>
     @endforeach
 @endif
 
 @if(session('success'))
-    <div class="alert alert-success">
+    <div class="alert alert-success image_display_text_container">
         {{session('success')}}
     </div>
 @endif
 
 @if(session('error'))
-    <div class="alert alert-danger">
+    <div class="alert alert-danger image_display_text_container">
         {{session('error')}}
     </div>
 @endif
