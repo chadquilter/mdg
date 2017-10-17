c42703748a50717a0f5fe5f2ce2974af7071c963
Chad Quilter <chadquilter@hotmail.com>
2017-10-16 21:09:08 -0500

    add new quote form pages

---
+++ resources/views/quotes/create.blade.php
@@ -0,0 +1,68 @@
+@extends('layouts.app')
+
+
+@section('content')
+  <div class="container">
+  	<div class="row">
+  		<br>
+  		<div class="col-md-6">
+  			<div class="image_display_r">
+  				<img class="img-fluid" alt="A scenic home image" src="/images/mdg_images/IMG_BATHROOM2.JPG" style="max-height:450%; width: 100%; display: block;">
+  				<div class="image_display_text_container">
+  					<p>Our work is a cut above the rest!</p>
+  				</div>
+  			</div>
+  		</div>
+  		<div class="col-md-6">
+  			<div class="image_display_r">
+  				<img class="img-fluid" alt="A scenic home image" src="/images/mdg_images/IMG_F1F2_1.JPG" style="max-height:450%; width: 100%; display: block;">
+  				<div class="image_display_text_container">
+  					<p>Our work is a cut above the rest!</p>
+  				</div>
+  			</div>
+  		</div>
+  	</div>
+  	<br>
+  	<div class="well">
+  		<h1>Create a Quote:</h1>
+  		{!! Form::open(['action' => 'QuotesController@store', 'method' => 'POST', 'enctype' => 'multipart/data']) !!}
+  			<div class="form=group">
+  					{{Form::label('quote_title', 'Quote Title:')}}
+  					{{Form::text('quote_title', '', ['class' => 'form-control', 'placeholder' => 'Quote Title'])}}
+  			</div>
+  			<div id="app-6" class="form=group">
+  					{{Form::label('quote_phone', 'Phone:')}}
+  					{{Form::text('quote_phone', '', ['class' => 'form-control', 'placeholder' => 'Phone'])}}
+  			</div>
+  			<div class="form=group">
+  					{{Form::label('quote_email', 'Email:')}}
+  					{{Form::text('quote_email', '', ['class' => 'form-control', 'placeholder' => 'Email'])}}
+  			</div>
+  			<div class="form=group">
+  					{{Form::label('job_summary', 'Summary:')}}
+  					{{Form::textarea('job_summary', 'Quote Summary Here...', ['id' => 'article-ckeditor', 'class' => 'form-control', 'placeholder' => 'Summary'])}}
+  			</div>
+  			<div class="form=group">
+  					{{Form::label('quote_notes', 'Quote Notes:')}}
+  					{{Form::text('quote_notes', '', ['class' => 'form-control', 'placeholder' => 'Quote Additional Notes'])}}
+  			</div>
+  			<br>
+  			<div class="form=group">
+  				{{Form::submit('Submit', ['class' => 'btn btn-primary'])}}
+  			</div>
+  		{!! Form::close() !!}
+  		<script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
+  		<script>
+  				CKEDITOR.replace( 'article-ckeditor' );
+  		</script>
+  	</div>
+  	<br>
+  	<div class="image_display_r">
+  		<img class="img-fluid"  alt="A scenic home image" src="/images/3_orig.jpg" style="max-height:450%; width: 100%; display: block;">
+  		<div class="image_display_text_container">
+  			<p>Custom Design!</p>
+  		</div>
+  	</div>
+  	<br>
+  </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/users/{id}/{name}', function($id, $name) {
 */
 
 Route::get('/', 'PagesController@index');
-Route::get('/quote', 'QuotesController@create');
+Route::resource('quote', 'QuotesController');
 Route::get('/services', 'PagesController@services');
 Route::resource('jobs', 'JobsController');
 Auth::routes();
