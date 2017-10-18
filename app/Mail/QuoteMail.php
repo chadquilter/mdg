d74ad6226dc452c6b094baa985d3805c59ae7aae
Chad Quilter <chadquilter@hotmail.com>
2017-10-18 01:55:05 -0500

    add quote object to quote mail controller

--- app/Mail/QuoteMail.php
+++ app/Mail/QuoteMail.php
@@ -33,7 +33,7 @@ class QuoteMail extends Mailable
     {
         return $this->view('emails.quote')
             ->with([
-                'quoteTitle' => $quote->title,
+                'quoteTitle' => $this->quote->title,
                 'quoteDescription' => 'sent from cut above construction55, hows it going?',
             ]);
     }
